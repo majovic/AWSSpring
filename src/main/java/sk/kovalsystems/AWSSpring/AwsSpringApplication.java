fa59407269451faa362d8505310cf1f4473cc552
„MarianH“ <hipik@kovalsystems.com>
2018-12-10 13:33:59 +0100

    Vezzia 1 preva inicializacia projektu

---
+++ src/main/java/sk/kovalsystems/AWSSpring/AwsSpringApplication.java
@@ -0,0 +1,12 @@
+package sk.kovalsystems.AWSSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSpringApplication.class, args);
+	}
+}
